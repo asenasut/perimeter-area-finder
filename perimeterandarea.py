74e09f1f2f5ba13946076253d8aee2750598a81f
asenasut <115923266+asenasut@users.noreply.github.com>
2022-11-11 22:39:17 +0300

    Add files via upload

---
+++ perimeterandarea.py
@@ -0,0 +1,7 @@
+#find the area and perimeter of a rectangle
+short=input('Short Edge : ')
+long=input('Long Edge : ')
+area=int(short)*int(long)
+perimeter=2*(int(short)+int(long))
+print("Area : {0}". format(area))
+print("Perimeter : {0}". format(perimeter))
